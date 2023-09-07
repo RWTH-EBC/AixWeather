c4e7c10b3afa4b701d4ecff69ee4ac1080de073e
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2023-09-07 08:20:21 +0200

    Revert "delete more tests"
    This reverts commit 7486aa25fc732bae43bcc146637f36934dc278a5.

---
+++ tests/test_EPW.py
@@ -0,0 +1,47 @@
+"""
+includes unittests for EPW data
+"""
+
+import os
+import unittest
+
+from aixweather.project_class import ProjectClassEPW
+from aixweather.core_data_format_2_output_file import utils_2output
+from config.definitions import ROOT_DIR
+from tests import utils_4_tests
+
+
+class BaseEPW(unittest.TestCase):
+    @classmethod
+    def init_and_run_EPW(cls, name: str, path: str):
+        abs_result_folder_path = os.path.join(utils_2output.result_folder_path(), name)
+        cls.c = ProjectClassEPW(
+            path=path, abs_result_folder_path=abs_result_folder_path
+        )
+        cls.folder_tests = os.path.join(
+            ROOT_DIR, f"tests/test_files/regular_tests/EPW/test_{name}"
+        )
+
+        utils_4_tests.run_all_functions(cls.c)
+
+        cls.start_formatted = cls.c.start.strftime("%Y%m%d")
+        cls.end_formatted = cls.c.end.strftime("%Y%m%d")
+        cls.station_id = "UnknownStationID"
+        cls.city = "Essen"
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        utils_4_tests.delete_created_result_files(cls.c.abs_result_folder_path)
+
+
+class TestEPWEssenLadybug(BaseEPW, utils_4_tests.RegressionTestsClass):
+    @classmethod
+    def setUpClass(cls):
+        cls.init_and_run_EPW(
+            "EPW_Essen_Ladybug",
+            os.path.join(
+                ROOT_DIR,
+                r"tests/test_files/regular_tests/EPW/test_EPW_Essen_Ladybug/"
+                "input/DEU_NW_Essen_104100_TRY2035_05_Wint_BBSR.epw",
+            ),
+        )
