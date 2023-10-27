48df05df01387d6b2fa0f5de3aad7a7a4eb50c64
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2023-10-27 19:14:49 +0200

    change resolution with data length

--- aixweather/data_quality_checks.py
+++ aixweather/data_quality_checks.py
@@ -17,19 +17,28 @@ def plot_heatmap_missing_values_daily(df):
     Returns:
         plt: A Matplotlib figure representing the heatmap of missing values.
     """
+
+    # define resolution depending on the length of the data set
+    if df.shape[0] <= 365:
+        resolution = "D"
+    elif df.shape[0] <= 3*365:
+        resolution = "W"
+    else:
+        resolution = "M"
+
     # Group by day and check for any missing values for each day
-    missing_data = df.resample("D").apply(lambda x: x.isnull().any())
+    missing_data = df.resample(resolution).apply(lambda x: x.isnull().any())
 
-    # Determine the number of days (rows) in your dataframe
-    num_days = missing_data.shape[0]
+    # Determine the number rows to plot
+    num_rows = missing_data.shape[0]
 
-    # Set the height of the figure based on the number of days, and a fixed width
-    plt.figure(figsize=(10, num_days * 0.15 + 3))
+    # Set the height of the figure based on the number of rows, and a fixed width
+    plt.figure(figsize=(10, num_rows * 0.15 + 3))
 
     sns.heatmap(missing_data, cmap="Greens_r", cbar=False)
 
     # Set y-tick labels to represent each day
-    plt.yticks(range(num_days), missing_data.index.date, rotation=0)
+    plt.yticks(range(num_rows), missing_data.index.date, rotation=0)
 
     plt.title("Heatmap of Missing Values (white = missing)")
     plt.tight_layout()
