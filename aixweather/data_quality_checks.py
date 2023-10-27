5d126026122c014bf484d4fe0446a608ce474b0e
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2023-10-27 19:47:07 +0200

    minor improvements at plot

--- aixweather/data_quality_checks.py
+++ aixweather/data_quality_checks.py
@@ -21,13 +21,13 @@ def plot_heatmap_missing_values_daily(df):
     # define resolution depending on the length of the data set
     if df.shape[0] <= 365:
         resolution = "D"
-    elif df.shape[0] <= 3*365:
+    elif df.shape[0] <= 3 * 365:
         resolution = "W"
     else:
         resolution = "M"
 
-    # Group by day and check for any missing values for each day
-    missing_data = df.resample(resolution).apply(lambda x: x.isnull().any())
+    # Group by resolution and check for missing values in each period
+    missing_data = df.resample(resolution).apply(lambda x: x.isnull().mean())
 
     # Determine the number rows to plot
     num_rows = missing_data.shape[0]
@@ -35,12 +35,16 @@ def plot_heatmap_missing_values_daily(df):
     # Set the height of the figure based on the number of rows, and a fixed width
     plt.figure(figsize=(10, num_rows * 0.15 + 3))
 
-    sns.heatmap(missing_data, cmap="Greens_r", cbar=False)
+    sns.heatmap(
+        missing_data,
+        cmap="Greens_r",
+        cbar=False,
+    )
 
     # Set y-tick labels to represent each day
     plt.yticks(range(num_rows), missing_data.index.date, rotation=0)
 
-    plt.title("Heatmap of Missing Values (white = missing)")
+    plt.title("Heatmap of Missing Values\nFrom white (no data) to dark green (100% data)")
     plt.tight_layout()
 
     return plt
