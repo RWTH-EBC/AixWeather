60dba77dba38925a5302d7168eba6aef9cf30922
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-04-10 16:42:21 +0200

    fix warning condition

--- aixweather/__init__.py
+++ aixweather/__init__.py
@@ -5,7 +5,7 @@ import pandas as pd
 from packaging.version import Version
 import warnings
 
-if Version(pd.__version__) > Version("2.1"):
+if Version(pd.__version__) >= Version("2.2"):
     warnings.warn("pandas versions >2.2 lead to wrong .eps and .mos results, consider downgrading to 2.1")
 
 __version__ = '0.1.6'
