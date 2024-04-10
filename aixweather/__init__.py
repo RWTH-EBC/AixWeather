0710d65980045aca2dd0729861e0fa8a476a6d85
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2024-04-10 13:59:27 +0200

    add warning for wrong version

--- aixweather/__init__.py
+++ aixweather/__init__.py
@@ -1,5 +1,11 @@
 """
 AixWeather-Module. See readme or documentation for more information.
 """
+import pandas as pd
+from packaging.version import Version
+import warnings
+
+if Version(pd.__version__) > Version("2.1"):
+    warnings.warn("pandas versions >2.2 lead to wrong .eps and .mos results, consider downgrading to 2.1")
 
 __version__ = '0.1.6'
