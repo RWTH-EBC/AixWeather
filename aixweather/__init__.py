b41cc8d7a6c3be3e9f99b4f0a1f4c0000ee1476d
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-05-02 18:09:38 +0200

    improve pylint

--- aixweather/__init__.py
+++ aixweather/__init__.py
@@ -1,11 +1,13 @@
 """
 AixWeather-Module. See readme or documentation for more information.
 """
-import pandas as pd
 from packaging.version import Version
 import warnings
 
+import pandas as pd
+
 if Version(pd.__version__) >= Version("2.2"):
-    warnings.warn("pandas versions >2.2 lead to wrong .eps and .mos results, consider downgrading to 2.1")
+    warnings.warn("pandas versions >2.2 lead to wrong .eps and .mos results,"
+                  " consider downgrading to 2.1")
 
 __version__ = '0.1.6'
