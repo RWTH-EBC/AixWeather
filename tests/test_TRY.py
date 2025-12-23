7f8a1af70a74b952461731590b244ebaa6231a0d
fabian.wuellhorst <fabian.wuellhorst@eonerc.rwth-aachen.de>
2025-12-23 11:48:58 +0100

    rm time-sleep

--- tests/test_TRY.py
+++ tests/test_TRY.py
@@ -19,11 +19,6 @@ from tests import utils_4_tests
 class BaseTRY(unittest.TestCase):
     @classmethod
     def init_and_run_TRY(cls, name: str, path: str, export_in_utc: bool, city: str):
-        # running the tests on the CI server with different python versions simultaneously causes,
-        # connection timeouts to nominatim, which is used to get the coordinates of the station.
-        # Therefore, we use a random timer to avoid this issue.
-        time.sleep(random.uniform(0, 20))
-
         abs_result_folder_path = os.path.join(definitions.result_folder_path(), name)
         cls.c = ProjectClassTRY(
             path=path, abs_result_folder_path=abs_result_folder_path
